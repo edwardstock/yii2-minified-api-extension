2176b6872384dad8aca3e5bbfda59dd5c46336eb
Eduard Maksimovich <edward.vstock@gmail.com>
2014-03-25 17:46:08 +0400

    Exceptions improvement. Added error info taken from constants like API_*

--- exceptions/MinifiedException.php
+++ exceptions/MinifiedException.php
@@ -6,13 +6,20 @@
  * Class: MinifiedException
  */
 
-namespace edwardstock\minified\exceptions;
+namespace edwardstock\minified\Exceptions;
 
 use yii\base\Exception;
 
 class MinifiedException extends Exception {
 
-	public function __construct($message, $code = 0, \Exception $_previous = null) {
+	protected $info;
+
+	public function __construct($message, $code = 0, \Exception $_previous = null, $info = null) {
+		$this->info = $info;
 		parent::__construct($message, $code, $_previous);
+	}
+
+	public function getInfo() {
+		return $this->info;
 	}
 } 
--- exceptions/MinifiedServiceException.php
+++ exceptions/MinifiedServiceException.php
@@ -9,21 +9,20 @@
 namespace edwardstock\minified\exceptions;
 
 
-use edwardstock\minified\Minified;
-
 class MinifiedServiceException extends MinifiedException {
 
 	public function __construct($message, $code = 0, \Exception $_previous = null) {
-		parent::__construct($message.' ERROR::'.$this->getMessageByCode($code), $code, $_previous);
+
+		parent::__construct($message, $code, $_previous, $this->getInfoByCode($code));
 	}
 
-	protected function getMessageByCode($code) {
+	protected function getInfoByCode($code) {
 		foreach((new \ReflectionClass('edwardstock\minified\Minified'))->getConstants() AS $name=>$value) {
 			if(preg_match('/(API_)(.*)/s', $name, $matches) && $value === $code) {
 				return $matches[2];
 			}
 		}
 
-		return 'NO_ERROR';
+		return 'UNCAUGHT_ERROR';
 	}
 } 
