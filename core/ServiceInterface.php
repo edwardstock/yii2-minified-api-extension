3bfba11fea9563031a19f6d21be59d694f69e44c
Eduard Maksimovich <edward.vstock@gmail.com>
2014-04-05 22:23:57 +0400

    Added comprehensive comments

--- core/ServiceInterface.php
+++ core/ServiceInterface.php
@@ -9,9 +9,21 @@ namespace EdwardStock\Minified\Core;
  */
 interface ServiceInterface {
 
+	/**
+	 * Get JSON data from api
+	 * @return mixed
+	 */
 	public function getData();
 
+	/**
+	 * Send JSON data to service api though POST method
+	 * @return mixed
+	 */
 	public function putData();
 
+	/**
+	 * Delete data on service api though DELETE method
+	 * @return mixed
+	 */
 	public function deleteData();
 } 
