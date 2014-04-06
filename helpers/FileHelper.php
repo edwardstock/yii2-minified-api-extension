ff6d9a2640d168c62f47cfd800759b454f40055e
Eduard Maksimovich <edward.vstock@gmail.com>
2014-04-07 02:24:36 +0400

    scanDirectory empty path error handle

--- helpers/FileHelper.php
+++ helpers/FileHelper.php
@@ -1,6 +1,9 @@
 <?php
 namespace EdwardStock\Minified\Helpers;
 
+use EdwardStock\Minified\Exceptions\MinifiedException;
+use yii\base\ErrorException;
+
 /**
  * minified. 2014
  * @author Eduard Maksimovich <edward.vstock@gmail.com>
@@ -34,10 +37,21 @@ class FileHelper {
 	/**
 	 * @param string $path
 	 * @param bool   $recursive
+	 * @throws \EdwardStock\Minified\Exceptions\MinifiedException
 	 * @return \SplFileInfo[]
 	 */
 	public static function scanDirectory($path, $recursive = true) {
 		\Yii::trace("Scanning directory. Recursive: {$recursive}", __METHOD__);
+
+		if (!is_dir($path)) {
+			try {
+				mkdir($path, 0775, true);
+			} catch(ErrorException $e) {
+				throw new MinifiedException("Directory $path is not exists and cannot be created. Check right for your web directory");
+			}
+
+		}
+
 		$flags = \FilesystemIterator::SKIP_DOTS | \FilesystemIterator::FOLLOW_SYMLINKS;
 
 		/** @var \SplFileInfo[] $objects */
